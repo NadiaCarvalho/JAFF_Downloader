a9b9dfda7eb73c2faac1a24fb9857762aaa9ba4b
Nádia Carvalho <nadiacarvalho118@gmail.com>
2019-03-15 11:38:56 +0000

    added script for aha complete

--- aha.py
+++ aha.py
@@ -22,24 +22,71 @@ args = parser.parse_args()
 
 
 # URLs
-url = 'http://meryton.com/aha/index.php?'
-login_url = url
-current_stories_url = url + '?board=13.0'
-complete_regency_stories_url = url + '?board=14.0'
-complete_modern_stories_url = url + '?board=97.0'
+login_url = 'http://meryton.com/aha/index.php?'
+login_post_url = "http://meryton.com/aha/index.php?app=core&module=global&section=login&do=process"
+url = args.url
 
 session_requests = requests.session()
 result = session_requests.get(login_url)
-tree = html.fromstring(result.text)
-authenticity_token = list(set(tree.xpath("//input[@name='auth_key']/@value")))[0]
+login_html = html.fromstring(result.text)
+authenticity_token = list(set(login_html.xpath("//input[@name='auth_key']/@value")))[0]
 
 payload = {
     "ips_username": args.user,
     "ips_password": args.password,
     "auth_key": authenticity_token
 }
-print(payload)
 
-response = session_requests.post("http://meryton.com/aha/index.php?app=core&module=global&section=login&do=process", data=payload, headers = dict(referer=login_url))
+response = session_requests.post(login_post_url, data=payload, headers = dict(referer=login_url))
 
-#TODO
+story_content = session_requests.get(url).content
+#print(story_content)
+
+tree = html.fromstring(story_content)
+
+pages_xpath = "//*[@class='topic_controls']//text()"
+pages = tree.xpath(pages_xpath)
+
+if(len(pages) > 5):
+    number_pages = pages[6].split(" ")[-2]
+else:
+    number_pages = 1
+
+title_xpath = "//*[@class='ipsType_pagetitle']/text()" 
+title = tree.xpath(title_xpath)
+title = title[0].strip('\n')
+
+file_title = title + ".txt"
+
+author_xpath = "//*[@itemprop='creator']//*[@itemprop='name']/text()"
+author_x = tree.xpath(author_xpath)
+author = author_x[0]
+
+title_x_y_path = "//*[@class='ipsType_pagetitle']//*[@class='desc']/text()" 
+title_x_y = tree.xpath(title_x_y_path)
+title_x_y = title_x_y[0]
+
+os.chdir("/Users/nadiacarvalho/Documents/Github/JAFF_DOWNLOADER/files/")
+file= open(file_title,'w')
+
+file.write("<h1>" + title + "</h1>\n")
+file.write("<h2>" + author + "</h2>\n")
+file.write("<h3>" + title_x_y + "</h3>\n\n\n")
+
+for i in range(0,int(number_pages)):
+    if "&st=" in url:
+        download_url = "".join(url.split("&st=")[:-1]) + "&st=" + str(i*2) + "0"
+    else:
+        download_url = url + "&st=" + str(i*2) + "0"
+    story_content = session_requests.get(download_url).content
+    story_tree = html.fromstring(story_content)
+    xpath = "//*[@class='post_body']//*[@itemprop='commentText']//text()"
+    page_posts = story_tree.xpath(xpath) 
+    for j in range(0,len(page_posts)):
+        if isinstance(page_posts[j],str):
+            file.write(page_posts[j])
+        else:
+            file.write(tostring(page_posts[j]))
+
+
+file.close() 
