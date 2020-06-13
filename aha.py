4533fb1fa761f5920986ef8980acbcacb5a9c5ab
NadiaCarvalho <nadiacarvalho118@gmail.com>
2020-06-13 23:14:49 +0100

    new changes to reflect the changes in css of the page

--- aha.py
+++ aha.py
@@ -12,62 +12,62 @@ from lxml.etree import tostring
 
 parser = argparse.ArgumentParser()
 parser.add_argument("url", help="url to download",
-                type=str)
+                    type=str)
 parser.add_argument("user", help="user of website",
-                type=str)
+                    type=str)
 parser.add_argument("password", help="password of website",
-                type=str)
+                    type=str)
 
 args = parser.parse_args()
 
-
 # URLs
-login_url = 'http://meryton.com/aha/index.php?'
-login_post_url = "http://meryton.com/aha/index.php?app=core&module=global&section=login&do=process"
+login_url = 'https://meryton.com/aha/index.php?/login/'
 url = args.url
 
 session_requests = requests.session()
 result = session_requests.get(login_url)
-login_html = html.fromstring(result.text)
-authenticity_token = list(set(login_html.xpath("//input[@name='auth_key']/@value")))[0]
+login_tree = html.fromstring(result.content)
+
+csrf_xpath = "//*[@class='ipsBox_alt']/input[@name='csrfKey']/@value"
+csrfKey = login_tree.xpath(csrf_xpath)[0]
 
 payload = {
-    "ips_username": args.user,
-    "ips_password": args.password,
-    "auth_key": authenticity_token
+    "csrfKey": csrfKey,
+    "auth": args.user,
+    "password": args.password,
+    "remember_me": 1,
+    "_processLogin": 'usernamepassword',
+    "_processLogin": 'usernamepassword',
 }
 
-response = session_requests.post(login_post_url, data=payload, headers = dict(referer=login_url))
+response = session_requests.post(login_url, data=payload)
 
 story_content = session_requests.get(url).content
 
 tree = html.fromstring(story_content)
 
-pages_xpath = "//*[@class='topic_controls']//text()"
+pages_xpath = "//*[@class='ipsPagination_pageJump']//text()"
 pages = tree.xpath(pages_xpath)
 
-if(len(pages) > 7):
-    number_pages = pages[6].split(" ")[-2]
+if(len(pages) > 2):
+    number_pages = pages[1].split(" ")[-2]
 else:
     number_pages = 1
 
-css_xpath = "//*[@id='full_version']"
-css_selected = tree.xpath(css_xpath)
-
-
-title_xpath = "//*[@class='ipsType_pagetitle']/text()" 
+title_xpath = "//h1//text()"
 title = tree.xpath(title_xpath)
-title = title[0].strip('\n')
+title = title[2].strip('\n')
 
-author_xpath = "//*[@itemprop='creator']//*[@itemprop='name']/text()"
+author_xpath = "//span[@class='ipsType_normal']//a//text()"
 author_x = tree.xpath(author_xpath)
 author = author_x[0]
 
-title_x_y_path = "//*[@class='ipsType_pagetitle']//*[@class='desc']/text()" 
-title_x_y = tree.xpath(title_x_y_path)
-title_x_y = title_x_y[0]
-    
-xpath = "//*[@class='post_body']//*[@itemprop='commentText']//text()"
+description_xpath = "//span[@class='cTemplateField_value']//text()" 
+description_x = tree.xpath(description_xpath)
+description = description_x[2]
+rating = description_x[5]
+    #comment-745041_wrap > div:nth-child(2)
+xpath = "//*[contains(concat(' ',normalize-space(@class),' '),'cPost_contentWrap')]//text()"
 
 file_title = title + ".txt"
 os.chdir("/Users/nadiacarvalho/Documents/Documents/FF/JAFF/files/")
@@ -75,7 +75,8 @@ file= open(file_title,'w')
 
 file.write("<h1>" + title + "</h1>\n")
 file.write("<h2>" + author + "</h2>\n")
-file.write("<h3>" + title_x_y + "</h3>\n\n\n")
+file.write("<h3>" + description + "</h3>\n")
+file.write("<h3>" + rating + "</h3>\n\n\n")
 
 num_chapters = 1
 
@@ -84,6 +85,7 @@ for i in range(0,int(number_pages)):
         download_url = "".join(url.split("&st=")[:-1]) + "&st=" + str(i*2) + "0"
     else:
         download_url = url + "&st=" + str(i*2) + "0"
+
     story_content = session_requests.get(download_url).content
     story_tree = html.fromstring(story_content)
     page_posts = story_tree.xpath(xpath) 
